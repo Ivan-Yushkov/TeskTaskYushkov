2c36c54a1431539d9227d86a8422bb4709848d68
Ivan <ivan.v.yushkov@gmail.com>
2020-12-03 21:07:06 +0300

    Initial Commit

---
+++ TeskTaskYushkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeskTaskYushkov
+//
+//  Created by Иван Юшков on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
